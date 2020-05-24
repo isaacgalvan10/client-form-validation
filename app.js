8b9df8259c5f036d31e4fca344153e24f373a6f5
isaacgalvan10 <galvan1522@gmail.com>
2020-05-24 12:55:01 -0500

    finished javascript validation

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
-const name = document.getElementById('name');
+// DOM Selectors
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
-const form = document.getElementsByTagName('form');
-const nameError = document.getElementById('name-error');
-const emailError = document.getElementById('email-error');
-const passwordError = document.getElementById('password-error');
+const rePassword = document.getElementById('re-password');
+
 
 form.addEventListener('submit', (e) => {
-  let messages = [];
-  if (name.value === '' || name.value == null) {
-    messages.push('Name is required');
+  e.preventDefault();
+
+  checkInputs();
+});
+
+// get values from the inputs
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const rePasswordValue = rePassword.value.trim();
+
+  if (usernameValue === '') {
+    setErrorFor(username, 'Username can not be blank');
+  } else if (usernameValue.length < 5) {
+    setErrorFor(username, 'Username must be more than 5 characters');
+  } else {
+    setSuccessFor(username)
   }
 
-if (password.valuelength <= 6) {
-  messages.push('Password must be longer than 6 characters');
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank')
+  } else if(!isEmail(emailValue)) {
+    setErrorFor(email, 'Email is not valid');
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'Password cannot be blank');
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (rePasswordValue === '') {
+    setErrorFor(rePassword, 'Re-enter Password');
+  } else if(rePasswordValue !== passwordValue) {
+    setErrorFor(rePassword, 'Passwords do not match')
+  } else {
+    setSuccessFor(rePassword);
+  }
 }
 
-  if (messages.length > 0) {
-    e.preventDefault();
-    nameError.innerText = messages.join(', ');
-  }
-})
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+
+  small.innerText = message;
+
+  formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
